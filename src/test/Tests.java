304244d6abca7abd4fbe8bb11b1c7ac5a98834b3
Santiago Ariel Mansilla <santiago_ariel_mansilla@hotmail.com>
2022-10-17 20:18:06 -0300

    .

---
+++ src/test/Tests.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+    @Test
+    private void testThis() {
+        Assertions.assertEquals(5, 2.5 + 2.5);
+    }
+}
