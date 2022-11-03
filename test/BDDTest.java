b6dfc3f3b1d56073960a5c47904e975f348fb7f2
GiaBab <gabrielbianglino@gmail.com>
2022-11-03 16:09:16 -0300

    Update BDDTest.java
    corrección de nombre y agregue un test mas

--- test/BDDTest.java
+++ test/BDDTest.java
@@ -1,49 +1,64 @@
-import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 import database.*;
+import enums.* ;
+import turnos.* ;
+import usuarios.Paciente;
 
 public class BDDTest {
 	
 	BDD baseDeDatos = BDD.getInstance();
-	
+
 	@Test
 	public void singleton() {
 		Assert.assertEquals(baseDeDatos, BDD.getInstance());
 	}
 	// Test inciailes para comprobar que todo inicia vacio (juntar los test luego en un method aparte)
+	
 	@Test
-	public void test1() {
+	public void inicializacionTest1() {
 		Assert.assertTrue(baseDeDatos.pacientes.getDatos().isEmpty());
 	}
     
 	@Test
-	public void test2() {
+	public void inicializacionTest2() {
 		Assert.assertTrue(baseDeDatos.prestaciones.getDatos().isEmpty());
 	}
 
 	@Test 
-	public void test3() {
+	public void inicializacionTest3() {
 		Assert.assertTrue(baseDeDatos.turnos.getDatos().isEmpty());
 	}
 
 	@Test 
-	public void test4() {
+	public void inicializacionTest4() {
 		Assert.assertTrue(baseDeDatos.turnosPendientes.getDatos().isEmpty());
 	}
 
 	@Test
-	public void test5() {
+	public void inicializacionTest5() {
 		Assert.assertTrue(baseDeDatos.laboratorios.getDatos().isEmpty());
 	}
 
 	@Test
-	public void test6() {
+	public void inicializacionTest6() {
 		Assert.assertTrue(baseDeDatos.idCBU.getDatos().isEmpty());
 	}
 
 	@Test
-	public void test7() {
+	public void inicializacionTest7() {
 		Assert.assertTrue(baseDeDatos.idEntidadFinanciacion.getDatos().isEmpty());
 	}
+
+	// probar poner las cosas que no corresponde de forma intencional 
+	Turno turno1 = new Turno(EstadoTurno.Aprobado);
+	Paciente juan = new Paciente(null, null) ;
+	@Test
+	public void tablaPacientesTienePacienteTest1() {
+		//baseDeDatos.pacientes.addDato(turno1);
+		baseDeDatos.pacientes.addDato(juan);
+		Assert.assertTrue(baseDeDatos.pacientes.getDatos().stream().allMatch(o -> o instanceof Paciente));
+	}
+	
 }
