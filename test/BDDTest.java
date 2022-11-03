d404327a6dcf698fca65ebb06e20ef13d5442802
GiaBab <gabrielbianglino@gmail.com>
2022-11-03 18:40:20 -0300

    Update BDDTest.java
    mejoras en BDDtest

--- test/BDDTest.java
+++ test/BDDTest.java
@@ -1,64 +1,45 @@
-
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import database.*;
 import enums.* ;
 import turnos.* ;
 import usuarios.Paciente;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class BDDTest {
 	
 	BDD baseDeDatos = BDD.getInstance();
 
-	@Test
+	@Test 
 	public void singleton() {
 		Assert.assertEquals(baseDeDatos, BDD.getInstance());
 	}
-	// Test inciailes para comprobar que todo inicia vacio (juntar los test luego en un method aparte)
 	
 	@Test
-	public void inicializacionTest1() {
-		Assert.assertTrue(baseDeDatos.pacientes.getDatos().isEmpty());
-	}
-    
-	@Test
-	public void inicializacionTest2() {
-		Assert.assertTrue(baseDeDatos.prestaciones.getDatos().isEmpty());
-	}
-
-	@Test 
-	public void inicializacionTest3() {
-		Assert.assertTrue(baseDeDatos.turnos.getDatos().isEmpty());
-	}
-
-	@Test 
-	public void inicializacionTest4() {
-		Assert.assertTrue(baseDeDatos.turnosPendientes.getDatos().isEmpty());
-	}
-
-	@Test
-	public void inicializacionTest5() {
-		Assert.assertTrue(baseDeDatos.laboratorios.getDatos().isEmpty());
-	}
-
-	@Test
-	public void inicializacionTest6() {
-		Assert.assertTrue(baseDeDatos.idCBU.getDatos().isEmpty());
-	}
-
-	@Test
-	public void inicializacionTest7() {
-		Assert.assertTrue(baseDeDatos.idEntidadFinanciacion.getDatos().isEmpty());
+	public void inicializacionTest() {
+		Assert.assertTrue(!baseDeDatos.pacientes.hayDatos());
+		Assert.assertTrue(!baseDeDatos.prestaciones.hayDatos());
+		Assert.assertTrue(!baseDeDatos.turnos.hayDatos());
+		Assert.assertTrue(!baseDeDatos.turnosPendientes.hayDatos());
+		Assert.assertTrue(!baseDeDatos.laboratorios.hayDatos());
+		Assert.assertTrue(!baseDeDatos.idEntidadFinanciacion.hayDatos());
 	}
 
 	// probar poner las cosas que no corresponde de forma intencional 
 	Turno turno1 = new Turno(EstadoTurno.Aprobado);
 	Paciente juan = new Paciente(null, null) ;
 	@Test
-	public void tablaPacientesTienePacienteTest1() {
+	public void tablaPacientesTienePacientesTest1() {
 		//baseDeDatos.pacientes.addDato(turno1);
 		baseDeDatos.pacientes.addDato(juan);
 		Assert.assertTrue(baseDeDatos.pacientes.getDatos().stream().allMatch(o -> o instanceof Paciente));
 	}
 	
+	@Test
+	public void tablaTurnosTieneTurnosTest1(){
+		GestionTurnos.registrarTurno(turno1);
+		Assert.assertTrue(baseDeDatos.turnos.getDatos().stream().allMatch(o -> o instanceof Turno));
+	}
 }
