449dfe4addaea017b74f0297e78dc4ec9ed5a89b
Santiago Ariel Mansilla <santiago_ariel_mansilla@hotmail.com>
2022-11-03 12:19:54 -0300

    Update Medico.java

--- src/usuarios/Medico.java
+++ src/usuarios/Medico.java
@@ -1,5 +1,27 @@
 package usuarios;
 
+import enums.EstadoFinanciero;
+import jdk.jshell.spi.ExecutionControl;
+import main.Diagnostico;
+import main.Estudio;
+import main.Medicacion;
+import main.Tratamiento;
+
+import java.util.List;
+
 public class Medico extends Usuario {
-    //TODO
+    public int idMedicoACargo;
+    public int idPaciente;
+    public Diagnostico diagnostico;
+    public boolean asistencia;
+    public EstadoFinanciero estadoFinanciero;
+
+    public List<Tratamiento> tratamientosAsignados;
+    public List<Medicacion> medicacionAsignada;
+    public List<Estudio> estudiosRealizados;
+
+    public float costoDePrestacion() throws ExecutionControl.NotImplementedException {
+        //TODO: Definir costo de prestación
+        throw new ExecutionControl.NotImplementedException("La función todavía no fue implementada.");
+    }
 }
