240c24bc073fc5dedcd0a9621ceded63aa4b8825
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2022-11-04 09:34:04 -0300

    Funcionalidad de enviar mail y sms agregada, además de agregar una nueva tabla a la base de datos llamada Usuarios

--- src/contacto/GestionContactos.java
+++ src/contacto/GestionContactos.java
@@ -1,9 +1,13 @@
 package contacto;
 import enums.EstadoTurno;
 import turnos.Turno;
+import database.*;
+import usuarios.*;
 
 public class GestionContactos {
     private static GestionContactos instance = null;
+    private BDD baseDeDatos = BDD.getInstance();
+    
     private GestionContactos() {}
 
     public static GestionContactos getInstance() {
@@ -13,10 +17,28 @@ public class GestionContactos {
     }
 
     public void enviarMailAlUsuario(Mensaje mensaje, int idUsuario) {
-        //TODO: Enviar mail al usuario
+        /*
+         	PROPÓSITO: Envia un mail al usuario con la id dada.
+         	PRECONDICION:
+         		* Debe existir un usario con la id dada.
+         	PARÁMETROS:
+         		* mensaje : Mensaje - El mensaje a enviarle al usuario.
+         		* idUsuario : int - La id del usuario a enviar el mensaje.
+        */
+    	Contacto contactoDelUsuario = baseDeDatos.usuarios.getDato(idUsuario).contacto;
+    	contactoDelUsuario.enviarAlCorreo(mensaje);
     }
 
-    public void enviarSMSAlUsuario(String mensaje, int idUsuario) {
-        //TODO: Envío de SMS
+    public void enviarSMSAlUsuario(Mensaje mensaje, int idUsuario) {
+        /*
+     		PROPÓSITO: Envia un SMS al usuario con la id dada.
+     		PRECONDICION:
+     			* Debe existir un usario con la id dada.
+     		PARÁMETROS:
+     			* mensaje : Mensaje - El mensaje a enviarle al usuario.
+     			* idUsuario : int - La id del usuario a enviar el mensaje.
+     	*/
+    	Contacto contactoDelUsuario = baseDeDatos.usuarios.getDato(idUsuario).contacto;
+    	contactoDelUsuario.enviarSMS(mensaje);   	
     }
 }
