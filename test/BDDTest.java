5eb2fa0d180346b574e7599c4cba35968b5bb669
GiaBab <gabrielbianglino@gmail.com>
2022-11-08 11:36:44 -0300

    test con null
    Se agrego test para los addDatos de que pasa si se le agrega un elemento null, como es de esperar tira un expect y no entra en las TablaDeDatos

--- test/BDDTest.java
+++ test/BDDTest.java
@@ -29,7 +29,7 @@ public class BDDTest {
 		Assert.assertFalse(hayDatosBDD);
 	}
 
-	@Test
+	@Test(expected = RuntimeException.class) 
 	public void tablaUsuarioTieneUsuarioTest1() {
 		String[] nombre1 = {"Juan"};
 		String[] apellido1 = {"Lopez", "Maria"} ;
@@ -38,39 +38,44 @@ public class BDDTest {
 		Administrativo admin = new Administrativo() ;
 		baseDeDatos.usuarios.addDato(admin);
 		baseDeDatos.usuarios.addDato(user1);
+		baseDeDatos.usuarios.addDato(null);
 		baseDeDatos.usuarios.addDato(juan);
 		Assert.assertTrue(baseDeDatos.usuarios.getDatos().stream().anyMatch(o -> o instanceof Administrativo));
 		Assert.assertTrue(baseDeDatos.usuarios.getDatos().stream().anyMatch(o -> o instanceof Paciente));
 		Assert.assertTrue(baseDeDatos.usuarios.getDatos().stream().allMatch(o -> o instanceof Usuario));
 	}
 	
-	@Test
+	@Test(expected = RuntimeException.class) 
 	public void tablaTurnosTieneTurnosTest1(){
 		Turno turno1 = new Turno(EstadoTurno.APROBADO);
 		GestionTurnos.registrarTurno(turno1);
 		baseDeDatos.turnos.addDato(turno1);
+		baseDeDatos.turnos.addDato(null);
 		Assert.assertEquals(2, baseDeDatos.turnos.cantidadDatos());
 		Assert.assertTrue(baseDeDatos.turnos.getDatos().stream().allMatch(o -> o instanceof Turno));
 	}
 
-	@Test
+	@Test(expected = RuntimeException.class) 
 	public void tablaprestacionesTienePrestacionTest1(){
 		Prestacion prestacion = new Prestacion(0) ;
+		baseDeDatos.prestaciones.addDato(null);
 		baseDeDatos.prestaciones.addDato(prestacion);
 		Assert.assertTrue(baseDeDatos.prestaciones.getDatos().stream().allMatch(o -> o instanceof Prestacion));
 	}
 
-	@Test 
+	@Test(expected = RuntimeException.class) 
 	public void tablaLaboratoriosTieneLaboratorioTest1(){
 		Laboratorio laboratorio = new Laboratorio() ;
 		baseDeDatos.laboratorios.addDato(laboratorio);
+		baseDeDatos.laboratorios.addDato(null);
 		Assert.assertTrue(baseDeDatos.laboratorios.getDatos().stream().allMatch(o -> o instanceof Laboratorio));
 	}
 
-	@Test
+	@Test(expected = RuntimeException.class) 
 	public void tablaIdEntidadFinanciacionTieneIdEntidadFinanciacion(){
 		EntidadFinanciera entidadFinanciera = new EntidadFinanciera() {};
 		baseDeDatos.idEntidadFinanciacion.addDato(entidadFinanciera);
+		baseDeDatos.idEntidadFinanciacion.addDato(null);
 		Assert.assertTrue(baseDeDatos.idEntidadFinanciacion.getDatos().stream().allMatch(o -> o instanceof EntidadFinanciera));
 	}
 
