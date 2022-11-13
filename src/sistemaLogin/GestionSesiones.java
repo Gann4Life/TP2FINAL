cb69e5d7f5aefa23460ccf995384d12ea53b424a
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2022-11-13 03:58:40 -0300

    Metodo generar token completado

--- src/sistemaLogin/GestionSesiones.java
+++ src/sistemaLogin/GestionSesiones.java
@@ -1,7 +1,22 @@
 package sistemaLogin;
 
+import java.util.Random;
+
+import database.BDD;
+import usuarios.Usuario;
+
 public class GestionSesiones {
     public static String generarToken(int id) {
-        return "aceituna";
+    	Usuario usuario = BDD.getInstance().usuarios.getDato(id);
+        return "#" + usuario.nombres[0] + codigoAleatorio();
+    }
+    
+    private static String codigoAleatorio() {
+    	Random random = new Random();
+    	String codigo = "";
+    	
+    	for(int i=0; i < 5; i++) codigo += random.nextInt(5);
+
+    	return codigo;
     }
 }
